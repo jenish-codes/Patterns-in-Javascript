5e7c8cf0e8dcde5b3f72b3d3e758cdf76c2b8b41
jenish-codes <jenish.codes@gmail.com>
2022-04-26 20:55:48 +0530

    created and working on js Patterns

---
+++ 01-js-pattern/script.js
@@ -0,0 +1,29 @@
+// Square star Pattern
+
+
+
+// let n = 5
+// let string1 = '';
+
+// for (let i=0; i<n; i++){
+//     //external loop
+//     for (let j=0; j<n; j++){
+//         //internal Loop
+//         string1 += '*';
+//     }
+//     //new line after each row
+//     string1 += '\n';
+// }
+// console.log(string1);
+
+
+
+
+
+
+
+
+
+
+
+//hollow square pattern
