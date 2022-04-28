653579331019c4d1cee33c89977f9c657157123f
jenish-codes <jenish.codes@gmail.com>
2022-04-28 19:58:03 +0530

    and another commit

--- 01-js-pattern/script.js
+++ 01-js-pattern/script.js
@@ -193,3 +193,82 @@
 
 //hollow pyramid
 
+// let n =5;
+// let string ='';
+// for (i=0; i<=n; i++){
+//     for (j=0; j<= n-i; j++){
+//         string += ' ';
+//     }
+//     for (k=0; k<2*i-1; k++){
+//         if(i===0 || i===n){
+//             string += '*';
+//         }
+//         else{
+//             if(k===0 || k===2*i-2){
+//                 string += '*';
+//             }
+//             else {
+//                 string += ' '
+//             }
+//         }
+//     }
+//     string+= '\n';
+// }
+// console.log(string)
+
+
+
+
+
+
+
+
+
+//Dimond patttern
+
+// let n=5;
+// let  string = '';
+// //upside pyramid
+// for(let i=1; i<=n; i++){
+//     for(let j=n; j>i; j--){
+//         string+= ' ';
+//     }
+//     for(let k=0; k<i*2-1; k++){
+//         string += '*';
+//     }
+//     string +='\n';
+// }
+// //downward pyramid
+// for(let i=1; i<=n-1; i++){
+//     for(let j=0; j<i; j++){
+//         string += ' ';
+//     }
+//     for(k=(n-i)*2-1; k>0; k--){
+//         string +='*';
+//     }
+//     string += '\n';
+// }
+// console.log(string);
+
+
+
+
+
+
+
+
+//hollow diamond
+
+
+
+
+
+
+
+
+
+
+
+
+
+
