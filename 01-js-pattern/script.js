27ebec7f601701466686ae1f2f5cab11d5ee9d67
jenish-codes <jenish.codes@gmail.com>
2022-04-29 12:21:00 +0530

    Compleated 15 star pattern in Js

--- 01-js-pattern/script.js
+++ 01-js-pattern/script.js
@@ -1,4 +1,4 @@
-// Square star Pattern
+//1  Square star Pattern
 
 
 
@@ -26,7 +26,7 @@
 
 
 
-//hollow square pattern
+// 2 hollow square pattern
 
 // let n=5; //rows or coloum count
 // let string = '';
@@ -54,7 +54,7 @@
 
 
 
-//right triangle pattern 
+// 3 right triangle pattern 
 
 
 // let n =5;
@@ -76,7 +76,7 @@
 
 
 
-//left triangle pattern
+// 4 left triangle pattern
 
 // let n=5;
 // let string= '';
@@ -98,7 +98,7 @@
 
 
 
-//downward triangle
+// 5 downward triangle
 
 // let n=5;
 // let string='';
@@ -118,7 +118,7 @@
 
 
 
-//hollow triangle star pattern
+// 6 hollow triangle star pattern
 
 // let n=6;
 // let string = '';
@@ -144,7 +144,7 @@
 
 
 
-//Pyramid pattern
+// 7 Pyramid pattern
 
 // let n=7;
 // let string ='';
@@ -170,7 +170,7 @@
 
 
 
-//reverse Pyramid
+// 8 reverse Pyramid
 
 // let n=5;
 // let string ='';
@@ -191,7 +191,7 @@
 
 
 
-//hollow pyramid
+// 9 hollow pyramid
 
 // let n =5;
 // let string ='';
@@ -224,7 +224,7 @@
 
 
 
-//Dimond patttern
+// 10 Dimond patttern
 
 // let n=5;
 // let  string = '';
@@ -257,7 +257,208 @@
 
 
 
-//hollow diamond
+// 11 hollow diamond
+
+// let n=5;
+// let string ='';
+// //upside pramid,dimond
+// for (let i=1; i<=n; i++){
+//     for (let j=n; j>i; j--){
+//         string += ' ';
+//     }
+//     //printing star
+//     for (let k=0; k<i*2-1; k++){
+//         if(k===0 || k===2*i-2){
+//             string +='*';
+//         }
+//         else {
+//             string += ' ';
+//         }
+//     }
+//     string +='\n';
+// }
+
+// //downward dimond
+// for (let i=1; i<=n-1; i++){
+//     for (let j=0; j<i; j++){
+//         string += ' ';
+//     }
+//     //printing star
+//     for (let k=(n-i)*2-1; k>=1; k--){
+//         if(k===1 || k===(n-i)*2-1){
+//             string += '*';
+//         }
+//         else {
+//             string += ' ';
+//         }
+//     }
+//     string +='\n';
+// }
+// console.log(string)
+
+
+
+
+
+
+
+
+
+
+
+
+// 12 Hourglass star
+
+
+// let n=5;
+// let string ='';
+
+// for (let i=0; i<n; i++){
+//     for(let j=0; j<i; j++){
+//         string += ' ';
+//     }
+//     for (let k=0; k<2*(n-i)-1; k++){
+//         string += '*';
+//     }
+//     string +='\n';
+// }
+// for (let i=2; i<=n; i++){
+//     for(let j=0; j<n-i; j++){
+//         string += ' ';
+//     }
+//     for(let k=0; k<2*i-1; k++){
+//         string += '*'
+//     }
+//     string += '\n';
+// }
+// console.log(string);
+
+
+
+
+
+
+
+
+
+
+
+
+//13 Right pascal star pattern
+
+// let n=5;
+// let string = '';
+
+// for(let i=1; i<=n; i++){
+//     for (let j=0; j<i; j++){
+//         string += '*';
+//     }
+//     string +='\n';
+// }
+// for (let i=1; i<=n-1; i++){
+//     for (let j=0; j<n-i; j++){
+//         string +='*';
+//     }
+//     string +='\n';
+// }
+// console.log(string);
+
+
+
+
+
+
+// 14 Left pascal star pattern
+
+// let n=5;
+// let string ='';
+
+// for (let i=1; i<=n; i++){
+//     for (let j=0; j<n-i; j++){
+//         string +=' ';
+//     }
+//     for (let k=0; k<i; k++){
+//         string += '*';
+//     }
+//     string += '\n';
+// }
+
+// for (let i=1; i<=n-1; i++){
+//     for (let j=0; j<i; j++){
+//         string += ' ';
+//     }
+//     for (let k=0; k<n-i; k++){
+//         string += '*';
+//     }
+//     string += '\n';
+// }
+// console.log (string);
+
+
+
+
+
+
+// 15 Heart Star pattern
+/*
+var n=6;
+var str ='';
+for (let i=n/2; i<n; i+=2) {
+    //printing first spaces
+    for(let j=1; j<n-i; j+=2){
+        str += ' ';
+    }
+    //printing firt star
+    for (let j=1; j<i+1; j++){
+        str +='*';
+    }
+    //print second space
+    for (let j=1; j<n-i+1; j++){
+        str += ' ';
+    }
+    //printing second star
+    for(let j=1; j<i+1; j++){
+        str += '*';
+    }
+    str += '\n';
+}
+//lower part
+//inverted pyramid
+for (let i=n; i>0; i--){
+    for (let j=0; j<n-i; j++){
+        str += ' ';
+    }
+    for (let j=1;  j<i*2; j++){
+        str +='*'
+    }
+    str += '\n';
+}
+console.log(str);
+
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
